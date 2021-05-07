187a3265f904af435520f742559bdbfa1b517632
Tim Elbert <tim.elbert@iem.thm.de>
2021-05-07 18:10:51 +0200

    Profile WS Injection

--- frontend-private/src/views/Profile.vue
+++ frontend-private/src/views/Profile.vue
@@ -9,21 +9,38 @@
         <td>isAdmin:</td>
         <td>{{ isAdmin }}</td>
       </tr>
+      <tr>
+        <td>Socket:</td>
+        <td>{{ connected ? 'Connected' : 'Disconnected' }}</td>
+      </tr>
     </table>
+    <button @click="test()">Test Connection</button>
   </div>
 </template>
 
 <script>
-  import { computed } from 'vue'
+  import { computed, inject } from 'vue'
   import { useStore } from 'vuex'
+  import { useSocket } from '@/helper'
 
   export default {
     setup(){
       const store = useStore()
+      const socket = useSocket()
+
+      socket.on('pong', (...data) => {
+        console.log(...data)
+      })
+
+      let test = () => {
+        socket.emit('ping', { ts: new Date() })
+      }
 
       return {
         username: computed(() => store.state.user.username),
         isAdmin: computed(() => store.state.user.isAdmin),
+        connected: socket.connected,
+        test,
       }
     }
   }
