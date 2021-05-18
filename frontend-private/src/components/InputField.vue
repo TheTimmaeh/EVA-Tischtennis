728ebe37a7826a675f0594b7cb3dc9e65643350c
Tim Elbert <tim.elbert@iem.thm.de>
2021-05-18 12:25:58 +0200

    Added logic to InputField

--- frontend-private/src/components/InputField.vue
+++ frontend-private/src/components/InputField.vue
@@ -1,23 +1,45 @@
 <template>
   <div>
-    <input class="" placeholder="Hinweis....">
+    <input :placeholder="placeholder" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
   </div>
 </template>
 
+<script>
+  export default {
+    name: 'InputField',
+    props: {
+      placeholder: {
+        type: String,
+        default: '',
+      },
+      modelValue: {
+        type: String,
+        default: '',
+      }
+    },
+    emits: {
+      'update:modelValue': null,
+    },
+    setup(props){
+      return {
+        ...props,
+      }
+    }
+  }
+</script>
+
 <style lang="scss" scoped>
-input{  
-	border: 1px solid $color-info;
-	padding: 7 9,5px;
-  border-radius: 5px; 
-  width: 450px;
-  height:38px;
-}
-input:focus{
-    outline: none !important;
-    border-color: $color-info;
-    box-shadow: 0 0 10px $color-info;
-}
-div{
-  padding: 15px;
-}
+  input {
+    border: 1px solid $color-info;
+    padding: 7 9,5px;
+    border-radius: 5px;
+    width: 450px;
+    height:38px;
+
+    &:focus {
+      outline: none !important;
+      border-color: $color-info;
+      box-shadow: 0 0 10px $color-info;
+    }
+  }
 </style>
