743133ea42713ffefec1f119b3a808da54f7b752
Tim Elbert <tim.elbert@iem.thm.de>
2021-05-18 12:27:13 +0200

    Project Cleanup

--- frontend-private/src/main.js
+++ frontend-private/src/main.js
@@ -2,8 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { initStore } from '@/helper'
+import { initStore, authHeader } from '@/helper'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+
+  // Vuex
+  .use(store)
+
+  // Vue Router
+  .use(router)
+
+  // Mount it
+  .mount('#app')
 
 initStore()
--- frontend-private/src/views/Home.vue
+++ frontend-private/src/views/Home.vue
@@ -1,32 +1,20 @@
 <template>
   <div class="home">
     EVA Tischtennis
-    <div>
-    </div>
   </div>
-  
 </template>
 
 <script>
-import { axios } from '@/helper'
-import { ref } from 'vue'
-import { useStore } from 'vuex'
-import { useRouter } from 'vue-router'
-
-
 export default {
   name: 'Home',
-  components:{},
   setup(){
     return { }
-  }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-.home{
-background-color: $color-fade-grey;
-}
-
+  .home {
+    background-color: $color-fade-grey;
+  }
 </style>
-
