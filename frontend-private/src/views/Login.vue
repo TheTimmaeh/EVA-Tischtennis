7976e16da8e3db4dd400ded5e54eb73561a741f6
Tim Elbert <tim.elbert@iem.thm.de>
2021-05-18 12:38:26 +0200

    Components in use

--- frontend-private/src/views/Login.vue
+++ frontend-private/src/views/Login.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
     <div class="message" v-if="message">{{ message }}</div>
-    <label for="username">Username:</label> <input id="username" name="username" type="text" v-model="username" /><br />
-    <label for="password">Password:</label> <input id="password" name="password" type="password" v-model="password" /><br />
-    <button @click="login()">Login</button>
+    <label for="username">Username:</label> <InputField id="username" type="text" v-model="username" /><br />
+    <label for="password">Password:</label> <InputField type="password" v-model="password" /><br />
+    <VButton @action="login()">Login</VButton>
   </div>
 </template>
 
@@ -12,9 +12,15 @@ import { axios } from '@/helper'
 import { ref } from 'vue'
 import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
+import InputField from '@/components/InputField'
+import VButton from '@/components/VButton'
 
 export default {
   name: 'Login',
+  components: {
+    InputField,
+    VButton,
+  },
   setup(){
     const store = useStore()
     const router = useRouter()
