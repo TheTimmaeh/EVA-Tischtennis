26b55fef4cbde56bb136a6a7d52c1991f1ad9be7
Tim Elbert <tim.elbert@iem.thm.de>
2021-05-23 17:49:36 +0200

    Added simple logic to Select component

--- frontend-private/src/components/Select.vue
+++ frontend-private/src/components/Select.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
     <select class="" placeholder="Hinweis....">
+      <option v-for="option in options" :key="option.key" :value="option.value">{{ option.text }}</option>
     </select>
   </div>
 </template>
 
+<script>
+  export default {
+    props: {
+      options: {
+        required: true,
+      },
+    },
+    setup(props){
+      return {
+        options: props.options,
+      }
+    },
+  }
+</script>
+
 <style lang="scss" scoped>
-select{  
+select{
 	width: 450px;
 	height: 25px;
 	border: 1px solid $color-info;
 	padding: 7 9,5px;
-  border-radius: 5px; 
+  border-radius: 5px;
 }
 select:focus{
     outline: none !important;
