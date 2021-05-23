2806ea876d0bb9a3fe718daa6a5f923415dc8169
Tim Elbert <tim.elbert@iem.thm.de>
2021-05-23 13:25:15 +0200

    Test Routes for Backend

--- backend/src/webserver/routes/index.js
+++ backend/src/webserver/routes/index.js
@@ -4,5 +4,14 @@ module.exports = (db) => {
   const authRoutes = require('./auth')(db)
   router.use('/auth', authRoutes)
 
+  // Test Routes
+  router.all('/*success', (req, res) => {
+    res.json({ success: true, method: req.method, path: req.path, data: req.body })
+  })
+
+  router.all('/*error', (req, res) => {
+    res.json({ success: false, message: 'An error has occured.', method: req.method, path: req.path, data: req.body })
+  })
+
   return router
 }
