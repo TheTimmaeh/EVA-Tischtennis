d66b7403a2c296adc18fdc665aabb0291b7c6897
Tim Elbert <tim.elbert@iem.thm.de>
2021-05-23 13:41:32 +0200

    DB changed to root user

--- backend/src/db/knexfile.js
+++ backend/src/db/knexfile.js
@@ -4,8 +4,10 @@ module.exports = {
     client: 'mysql',
     connection: {
       host: '127.0.0.1',
-      user: 'knex',
-      password: 'knex',
+      // user: 'knex',
+      // password: 'knex',
+      user: 'root',
+      password: 'example',
       database: 'eva_tischtennis',
     },
     migrations: {
