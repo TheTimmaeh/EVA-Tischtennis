b2b0c519c9f19f60df3428ae919db1e33f4bbad6
Tina <tina.haensel@iem.thm.de>
2021-05-24 15:44:25 +0200

    Added Seasons Views

---
+++ frontend-private/src/views/Seasons/Create.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="seasons">
+    <div class="message" v-if="message">{{ message }}</div>
+    <Form :rows="rows" @onValid="submit($event)" @onInvalid="invalid($event)" />
+  </div>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  import { axios, validate } from '@/helper'
+  import Form from '@/components/Form'
+
+  export default {
+    name: 'CreateSeason',
+    components: {
+      Form,
+    },
+    setup(){
+      const router = useRouter()
+      const message = ref('')
+
+      const submit = (data) => {
+        message.value = ''
+
+        axios().post('/seasons/create/success', data).then((res) => {
+          if(!res.data){
+            message.value = 'Unknown Error.'
+          } else if(!res.data.success){
+            message.value = res.data.message
+          } else {
+            message.value = 'Saison wurde angelegt.'
+            console.log(res.data)
+            setTimeout(() => router.push({ path: '/seasons' }), 3000)
+          }
+        }).catch((err) => {
+          message.value = err
+        })
+      }
+
+      const invalid = () => {
+        message.value = 'Bitte überprüfen Sie Ihre Eingabe.'
+      }
+
+      return {
+        submit,
+        invalid,
+        message,
+        rows: [
+          { name: 'year', text: 'Jahr der Saison:', field: 'input', type: 'number', min: 1874, max: new Date().getFullYear(), validate: { type: validate.types.year, required: true } },
+          { name: 'summerOrWinter', text: 'Saison:', field: 'seasonSelect' }, // TODO: Der Wert der Season wird bisher noch nciht an das Formular übergeben
+        ],
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ frontend-private/src/views/Seasons/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="saisons">
+    Liste zur Saisonübersicht
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'Saisons',
+  components: { },
+  setup(){
+    return { }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
