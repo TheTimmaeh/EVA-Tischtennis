6af17c6b574e6cf35ed1c6fd89a8254277f8d526
Tina <tina.haensel@iem.thm.de>
2021-05-24 15:46:13 +0200

    Added Component CheckBox

---
+++ frontend-private/src/components/CheckBox.vue
@@ -0,0 +1,47 @@
+<template>
+  <input type="checkbox"  v-model="checkedVals"
+    :value="checkedVal"
+    >
+</template>
+
+
+<script>
+
+
+export default {
+    name:'CheckBox',
+    props: {
+      label: {
+        type: String,
+      },
+    },
+    emits: {
+      'update:modelValue': null,
+    },
+    data() {
+      return { checkedVal:Boolean}
+    },
+    methods: {
+      updateVals(e) {
+        let isChecked = e.target.checked
+        let val = e.target.value
+
+        if (isChecked) {
+          this.checkedVal = true
+        } else {
+          this.checkVal = false
+        }
+      }
+    }
+  }
+</script>
+
+<style  lang="scss" scoped>
+
+input{
+    height: 30px;
+    width: 30px;
+    border: 1px solid $color-info;
+}
+
+</style>
