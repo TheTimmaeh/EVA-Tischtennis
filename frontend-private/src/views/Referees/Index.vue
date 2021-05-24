6560b6a09dc3250c210932541c682fd4e6387319
Tina <tina.haensel@iem.thm.de>
2021-05-24 15:44:41 +0200

    Added Referees views

---
+++ frontend-private/src/views/Referees/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="referees">
+    Liste aller Schiedsrichter 
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'Referees',
+  components: { },
+  setup(){
+    return { }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
