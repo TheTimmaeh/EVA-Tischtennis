31d663b74929b6c09b916b7d2d4afc23ec291229
Tina <tina.haensel@iem.thm.de>
2021-05-24 15:50:17 +0200

    Added competitions routes to backend

---
+++ backend/src/webserver/routes/competitions.js
@@ -0,0 +1,37 @@
+const router = require('express').Router()
+const { generateToken, authenticateToken } = require('../../helpers/jwt')
+
+module.exports = (db) => {
+
+  // Index
+  router.get('/', async (req, res) => {
+    res.json({})
+  })
+
+  // Create
+  router.post('/', authenticateToken, async (req, res) => {
+    res.json({})
+  })
+
+  // Get (one competition)
+  router.get('/:competition', async (req, res) => {
+    res.json({})
+  })
+
+  // Get (list of competitions)
+  router.get('/:competitions', async (req, res) => {
+    res.json({})
+  })
+
+  // Update
+  router.post('/:competition', authenticateToken, async (req, res) => {
+    res.json({})
+  })
+
+  // Delete
+  router.delete('/:competition', authenticateToken, async (req, res) => {
+    res.json({})
+  })
+
+  return router
+}
