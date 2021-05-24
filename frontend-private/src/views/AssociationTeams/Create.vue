b6c3cd1b009c69f7ebac7e50c2b201a40c8087d9
Tina <tina.haensel@iem.thm.de>
2021-05-24 15:45:45 +0200

    Added AssociationTeams views

---
+++ frontend-private/src/views/AssociationTeams/Create.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="associationTeams">
+    <div class="message" v-if="message">{{ message }}</div>
+    <Form :rows="rows" @onValid="submit($event)" @onInvalid="invalid($event)" />
+  </div>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  import { axios, validate } from '@/helper'
+  import Form from '@/components/Form'
+
+  export default {
+    name: 'CreateAssociationTeam',
+    components: {
+      Form,
+    },
+    setup(){
+      const router = useRouter()
+      const message = ref('')
+
+      const submit = (data) => {
+        message.value = ''
+
+        axios().post('/associationTeams/create/success', data).then((res) => {
+          if(!res.data){
+            message.value = 'Unknown Error.'
+          } else if(!res.data.success){
+            message.value = res.data.message
+          } else {
+            message.value = 'Vereinsmannschaft wurde angelegt.'
+            console.log(res.data)
+            setTimeout(() => router.push({ path: '/associationsTeams' }), 3000)
+          }
+        }).catch((err) => {
+          message.value = err
+        })
+      }
+
+      const invalid = () => {
+        message.value = 'Bitte überprüfen Sie Ihre Eingabe.'
+      }
+
+      return {
+        submit,
+        invalid,
+        message,
+        rows: [
+          { name: 'nameAssociationClass', text: 'Name der Vereinsmannschaft:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } },
+          { name: 'nameAssociation', text: 'Name des Vereins:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } }, //TODO: Das später noch als DropDown umsetzen, welches die unterschiedlichen beriets existierenden Vereine anbietet, oder die Möglichkeit eine Vereinsmannschaft anzulegen nur über den Verein selbst anbieten, das müssen wir zunächst noch entscheiden, letzteres finde ich sinnvoller
+        ],
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ frontend-private/src/views/AssociationTeams/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="associationTeams">
+    Liste der Vereinsamnnschaften
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'PlayerClasses',
+  components: { },
+  setup(){
+    return { }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
