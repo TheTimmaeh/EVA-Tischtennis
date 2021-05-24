f6992b3d84f22d2c049e2c7a2377f26fb30e0651
Tina <tina.haensel@iem.thm.de>
2021-05-24 15:45:06 +0200

    Added Member views

---
+++ frontend-private/src/views/Members/Create.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="members">
+    <div class="message" v-if="message">{{ message }}</div>
+    <Form :rows="rows" @onValid="submit($event)" @onInvalid="invalid($event)" />
+  </div>
+</template>
+
+<script>
+  import { ref } from 'vue'
+  import { useRouter } from 'vue-router'
+  import { axios, validate } from '@/helper'
+  import Form from '@/components/Form'
+
+  export default {
+    name: 'CreateMember',
+    components: {
+      Form,
+    },
+    setup(){
+      const router = useRouter()
+      const message = ref('')
+
+      const submit = (data) => {
+        message.value = ''
+
+        axios().post('/members/create/success', data).then((res) => {
+          if(!res.data){
+            message.value = 'Unknown Error.'
+          } else if(!res.data.success){
+            message.value = res.data.message
+          } else {
+            message.value = 'Vereinsmitglied wurde angelegt.'
+            console.log(res.data)
+            setTimeout(() => router.push({ path: '/members' }), 3000)
+          }
+        }).catch((err) => {
+          message.value = err
+        })
+      }
+
+      const invalid = () => {
+        message.value = 'Bitte überprüfen Sie Ihre Eingabe.'
+      }
+
+      return {
+        submit,
+        invalid,
+        message,
+        rows: [
+          { name: 'member', text: 'Mitglied', field: 'h2' },
+          { name: 'name', text: 'Vorname:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } },
+          { name: 'surname', text: 'Nachname:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } },
+          { name: 'year', text: 'Geburtsdatum:', field: 'input', type: 'text', validate: { type: validate.types.date, required: true } },
+          { name: 'verein', text: 'Verein:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } }, //TODO: Hier ein DropDown einbinden, dass die angelegten Vereine anbietet. Dann muss allerdings immer zuerst der Verein angelegt werden und dann das Mitglied. Wollen wir das?
+          { name: 'board', text: 'Vorstand:', field: 'isDirector' }, //TODO: Der Wert wird noch nicht an das Formular übergeben
+          { name: 'class', text: 'Spielerklasse:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } }, //TODO: Hier ein DropDown einbinden, dass alle Spielerklassen anbietet, inkl "keine"
+          { name: 'team', text: 'Vereinsmannschaft:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } }, //TODO: Das als Dropdown mit den Möglichkeiten einbinden
+          
+          { name: 'adress', text: 'Anschrift', field: 'h2' },
+          { name: 'street', text: 'Straße:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } },
+          { name: 'streetnumber', text: 'Hausnummer:', field: 'input', type: 'text', validate: { type: validate.types.streetnumber, required: true } },
+          { name: 'zipcode', text: 'PLZ:', field: 'input', type: 'text', validate: { type: validate.types.zipcode, required: true } },
+          { name: 'city', text: 'Ort:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } }, 
+          { name: 'state', text: 'Bundesland:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } }, //TODO: ersetzen durch DropDown dass die Bundesländer anbietet
+          { name: 'country', text: 'Land:', field: 'input', type: 'text', validate: { min: 3, max: 255, required: true } },//TODO: ersetzen durch DropDOWN, dass die Länder anbietet
+        
+          { name: 'konkakt', text: 'Kontakt', field: 'h2' },
+          { name: 'phone', text: 'Telefon:', field: 'input', type: 'tel', validate: { type: validate.types.phone, required: true } },
+          { name: 'mail', text: 'E-Mail:', field: 'input', type: 'email', validate: { type: validate.types.mail, required: true } },
+        ],
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ frontend-private/src/views/Members/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="members">
+    Liste aller Vereinsitglieder 
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'Members',
+  components: { },
+  setup(){
+    return { }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
