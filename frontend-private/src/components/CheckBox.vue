651ddde719c3317d56cba7148f15488fe3161c1e
Tim Elbert <tim.elbert@iem.thm.de>
2021-05-28 11:51:37 +0200

    Fixed Checkbox Logic

--- frontend-private/src/components/CheckBox.vue
+++ frontend-private/src/components/CheckBox.vue
@@ -1,32 +1,47 @@
 <template>
-  <input type="checkbox"  
-     id="checkbox" v-model="checked"
-    >
+  <input
+    type="checkbox"
+    :id="props.label"
+    :value="props.returnValue"
+    :checked="props.modelValue === props.returnValue"
+    @input="$emit('update:modelValue', $event.target.checked ? props.returnValue : null)"
+  />
 </template>
 
-
 <script>
-export default {
-    name:'CheckBox',
-    data () {
-      return {
-        checked: false,
-      }
-    },
+  export default {
+    name: 'CheckBox',
     props: {
+      modelValue: {
+        type: String,
+        default: null,
+      },
       label: {
         type: String,
+        default: '',
       },
+      returnValue: {
+        type: String,
+        default: 'true',
+      }
+    },
+    emits: {
+      'update:modelValue': null,
+    },
+    setup(props){
+      return {
+        props,
+        checked: false,
+      }
     },
   }
 </script>
 
-<style  lang="scss" scoped>
-
-input{
+<style lang="scss" scoped>
+  input {
     height: 30px;
     width: 30px;
+    margin: 5px 0;
     border: 1px solid $color-info;
-}
-
+  }
 </style>
