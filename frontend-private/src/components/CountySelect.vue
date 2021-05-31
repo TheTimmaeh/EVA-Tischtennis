ed73bb39368479a32208e9dc49fbc1951d294fc1
Tina <tina.haensel@iem.thm.de>
2021-05-31 17:43:12 +0200

    Added County-Select of EU-Memebers as Component

---
+++ frontend-private/src/components/CountySelect.vue
@@ -0,0 +1,79 @@
+<template>
+    <Select :options="props.options" :label="props.label" :error="error" v-model="selectValue" />
+</template>
+
+<script>
+  import { ref, watch } from 'vue'
+  import Select from '@/components/Select'
+
+  export default {
+    name: 'CountrySelect',
+    props: {
+      options: {
+        type: Array, // EU members
+        default: [
+          { value: '', text: '' },
+          { value: 'AT', text: 'Österreich' },
+          { value: 'BE', text: 'Belgien' },
+          { value: 'BG', text: 'Bulgarien' },
+          { value: 'CY', text: 'Zypern' },
+          { value: 'CZ', text: ' Tschechien' },
+          { value: 'DE', text: ' Deutschland' },
+          { value: 'DK', text: ' Dänemark' },
+          { value: 'EE', text: ' Estland' },
+          { value: 'ES', text: ' Spanien' },
+          { value: 'FI', text: ' Finnland' },
+          { value: 'FR', text: ' Frankreich' },
+          { value: 'GR', text: ' Griechenland' },
+          { value: 'HR', text: ' Kroatien' },
+          { value: 'HU', text: ' Ungarn' },
+          { value: 'IE', text: ' Irland' },
+          { value: 'IT', text: ' Italien' },
+          { value: 'LT', text: ' Litauen' },
+          { value: 'LU', text: ' Luxemburg' },
+          { value: 'LV', text: ' Lettland' },
+          { value: 'MT', text: ' Malta' },
+          { value: 'NL', text: ' Niederlande' },
+          { value: 'PL', text: ' Polen' },
+          { value: 'PT', text: ' Portugal' },
+          { value: 'RO', text: ' Rumänien' },
+          { value: 'SE', text: ' Schweden' },
+          { value: 'SI', text: ' Slowenien' },
+          { value: 'SK', text: ' Slowakei' },
+        ],
+      },
+      label: {
+        type: String,
+        default: '',
+      },
+      modelValue: {
+        type: String,
+        default: '',
+      },
+      error: {
+        type: [Boolean, String],
+        default: false,
+      },
+    },
+    components: {
+      Select,
+    },
+    emits: {
+      'update:modelValue': null,
+    },
+    setup(props, { emit }){
+      const error = ref(props.error === 'true' ? true : props.error)
+      const selectValue = ref('')
+
+      watch(selectValue, () => {
+        emit('update:modelValue', selectValue.value)
+      })
+
+      return {
+        error,
+        selectValue,
+        props,
+      }
+    },
+  }
+</script>
