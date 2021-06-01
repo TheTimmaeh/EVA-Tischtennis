e6bfad7a83bcefc8a80f55aea408ec93ecf9540d
Tim Elbert <tim.elbert@iem.thm.de>
2021-06-01 14:20:46 +0200

    New format for backend routes

--- backend/src/webserver/routes/competitions.js
+++ backend/src/webserver/routes/competitions.js
@@ -1,32 +1,48 @@
 const router = require('express').Router()
 const { generateToken, authenticateToken } = require('../../helpers/jwt')
+let db
 
-module.exports = (db) => {
+module.exports = (_db) => {
+  db = _db
 
   // Index
-  router.get('/', async (req, res) => {
-    res.json({})
-  })
-
-  // Create
-  router.post('/', authenticateToken, async (req, res) => {
-    res.json({})
-  })
+  router.get('/', getAllCompetitions)
 
   // Get (one competition)
-  router.get('/:competition', async (req, res) => {
-    res.json({})
-  })
+  router.get('/:competition', getCompetition)
+
+  // Create
+  router.post('/', authenticateToken, createCompetition)
 
   // Update
-  router.post('/:competition', authenticateToken, async (req, res) => {
-    res.json({})
-  })
+  router.post('/:competition', authenticateToken, updateCompetition)
 
   // Delete
-  router.delete('/:competition', authenticateToken, async (req, res) => {
-    res.json({})
-  })
+  router.delete('/:competition', authenticateToken, deleteCompetition)
 
   return router
+}
+
+async function getAllCompetitions(req, res){
+  // let competitions = (await db.select().from('competitions'))
+  //
+  // res.json({competitions})
+
+  res.json({})
+}
+
+async function getCompetition(req, res){
+  res.json({})
+}
+
+async function createCompetition(req, res){
+  res.json({})
+}
+
+async function updateCompetition(req, res){
+  res.json({})
+}
+
+async function deleteCompetition(req, res){
+  res.json({})
 }
