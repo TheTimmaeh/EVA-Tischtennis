a780bb575b79a388334e926f6124ec64d6d271a0
Tina <tina.haensel@iem.thm.de>
2021-06-01 15:53:12 +0200

    Seasons CRUD

--- backend/src/webserver/routes/seasons.js
+++ backend/src/webserver/routes/seasons.js
@@ -5,20 +5,22 @@ module.exports = (db) => {
 
   // Index
   router.get('/', async (req, res) => {
-    res.json({})
+    let seasons = getAllSeasons(req, res)
+    res.json({seasons})
   })
 
   // Create
   router.post('/', authenticateToken, async (req, res) => {
+    createSeason(req, res)
     res.json({})
   })
 
-  // Get (one saison)
+  // Get (one saison) -> Ich glaube das brauchen wir gar nicht
   router.get('/:season', async (req, res) => {
     res.json({})
   })
 
-  // Update
+  // Update -> Ich glaube das brauchen wir auch nicht
   router.post('/:season', authenticateToken, async (req, res) => {
     res.json({})
   })
@@ -29,4 +31,20 @@ module.exports = (db) => {
   })
 
   return router
+}
+
+
+async function getAllSeasons(req, res){
+  let referees = (await db.select().from('season')) 
+  res.json({referees})
+}
+
+async function createSeason(req, res){
+  db('season').insert([{year: req.year}, {season: req.season},])
+  res.json({})
+}
+
+async function deleteSeason(req, res){
+  await db('season').where([{'year': req.year}, {'season': req.season} ]).del()
+  res.json({})
 }
