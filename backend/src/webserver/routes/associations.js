25a7c89bc5f74436c928d43d9e846b4bd0c57d36
Tina <tina.haensel@iem.thm.de>
2021-06-01 16:16:00 +0200

    Associations CRUD

--- backend/src/webserver/routes/associations.js
+++ backend/src/webserver/routes/associations.js
@@ -5,28 +5,90 @@ module.exports = (db) => {
 
   // Index
   router.get('/', async (req, res) => {
-    res.json({})
+    let Associations = getAllAssociations(req, res)
+    res.json({Associations})
   })
 
   // Create
   router.post('/', authenticateToken, async (req, res) => {
+    createAssociation(req, res)
     res.json({})
   })
 
   // Get (one association)
   router.get('/:association', async (req, res) => {
-    res.json({})
+    let association = getAssociation(req, res)
+    res.json({association})
   })
 
   // Update
   router.post('/:association', authenticateToken, async (req, res) => {
+    updateAssociation(req, res)
     res.json({})
   })
 
   // Delete
   router.delete('/:association', authenticateToken, async (req, res) => {
+    deleteAssociation(req, res)
     res.json({})
   })
 
   return router
+}
+
+
+async function getAllAssociations(req, res){
+  let referees = (await db.select().from('associations')) 
+  res.json({referees})
+}
+
+async function getAssociation(req, res){
+  let referee = (await db.select().from('associations').where('name', req.name)) 
+  res.json({referee})
+}
+
+async function createAssociation(req, res){
+  db('associations').insert([
+                              {name: req.name}, 
+                              {year: req.year},
+                              {location:req.location},
+                              {description:req.description},
+                              {address:req.address},{street:req.street},
+                              {streetnumber:req.streetnumber},
+                              {zipcode:req.zipcode},
+                              {city:req.city},
+                              {state:req.state},
+                              {country:req.country},
+                              {contact:req.contact},
+                              {board:req.board},
+                              {phone:req.phone},
+                              {mail:req.mail},
+                              {website:req.website},
+                            ])
+  res.json({})
+}
+
+async function updateAssociation(req, res){
+  db('associations').where('name', req.name).update([
+                                                      {year: req.year},
+                                                      {location:req.location},
+                                                      {description:req.description},
+                                                      {address:req.address},{street:req.street},
+                                                      {streetnumber:req.streetnumber},
+                                                      {zipcode:req.zipcode},
+                                                      {city:req.city},
+                                                      {state:req.state},
+                                                      {country:req.country},
+                                                      {contact:req.contact},
+                                                      {board:req.board},
+                                                      {phone:req.phone},
+                                                      {mail:req.mail},
+                                                      {website:req.website},
+                                                    ])
+  res.json({})
+}
+
+async function deleteAssociation(req, res){
+  await db('associations').where('name', req).del()
+  res.json({})
 }
