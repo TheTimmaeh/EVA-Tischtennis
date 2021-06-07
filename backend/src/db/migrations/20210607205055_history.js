4ccc653192213905f7842cc0fb0380707a8a06c8
ArdisX <daniel.wallner@mnd.thm.de>
2021-06-07 22:56:44 +0200

    History Tabelle erstellt

---
+++ backend/src/db/migrations/20210607205055_history.js
@@ -0,0 +1,15 @@
+exports.up = (knex) => {
+  return knex.schema.createTable('history', (history) => {
+    history.increments('id')
+    history.string('event_type', 255).unique().notNullable()
+    history.string('reference1', 255).notNullable()
+    history.string('reference2', 255).notNullable()
+	history.integer('user').references('users.id').notNullable()
+    history.timestamps(true, true)
+    console.info('DB | Migration: history | Table history created.')
+  })
+}
+
+exports.down = (knex) => {
+  return knex.schema.dropTable('history')
+}
