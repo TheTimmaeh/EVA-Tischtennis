d0bb236eb592b5fad9d1435ead40458ba4faa69b
Tim Elbert <tim.elbert@iem.thm.de>
2021-06-08 13:39:15 +0200

    Fixed foreign key

--- backend/src/db/migrations/20210607205055_history.js
+++ backend/src/db/migrations/20210607205055_history.js
@@ -4,7 +4,7 @@ exports.up = (knex) => {
     history.string('event_type', 255).unique().notNullable()
     history.string('reference1', 255).notNullable()
     history.string('reference2', 255).notNullable()
-	history.integer('user').references('users.id').notNullable()
+    history.integer('user').unsigned().references('users.id').notNullable()
     history.timestamps(true, true)
     console.info('DB | Migration: history | Table history created.')
   })
