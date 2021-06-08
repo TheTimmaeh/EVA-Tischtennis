eb1a1dbbfe79e10df2432d6787247da6c6a652e9
Tim Elbert <tim.elbert@iem.thm.de>
2021-06-08 15:08:19 +0200

    Created Card Component

---
+++ frontend-private/src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Card',
+  }
+</script>
+
+<style lang="scss" scoped>
+  div {
+    background-color: white;
+    color: $color-dark-text;
+    padding: 10px 25px;
+    text-align: left;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    border: 2px solid $color-info;
+    border-radius: 5px;
+    margin: 8px;
+    overflow: hidden;
+    position: relative;
+
+    &.warning {
+      background-color: $color-warning;
+      color: $color-dark-text;
+    }
+
+    &.danger {
+      background-color: $color-danger;
+    }
+
+    &:hover {
+      box-shadow: 0 4px 4px 0 $color-info;
+    }
+  }
+</style>
