31a6fbae0e8a4866228242ecff639cece6f7aee5
Tina <tina.haensel@iem.thm.de>
2021-06-09 12:39:19 +0200

    Added Card Component to SeasonCard

--- frontend-private/src/components/SeasonCard.vue
+++ frontend-private/src/components/SeasonCard.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="card ">
-      <div class="header">
-        <div class="primary-title">
-          <div class="title">{{season}}</div>
-          <div class="title">{{year}}</div>
-          <Button>Bearbeiten</Button>
-        </div>
-      </div>
-  </div>
+  <Card>
+    <tr>
+      <td>Spieler: {{season}}</td>
+      <td>Verein: {{year}}</td>
+      <td class='min'>
+        <Button>Bearbeiten</Button>
+      </td>
+    </tr>
+  </Card>
 </template>
 
 <script>
 
-import Button from "./Button";
+import Button from "./Button"
+import Card from './Card.vue'
 import { ref } from 'vue'
 
 export default {
   name: 'SeasonCard',
-  components:{Button},
+  components:{
+    Button,
+    Card,
+    },
   setup(){
     let season = ref('Saison konnte nicht geladen werden')
     let year = ref('Jahr konnte nicht geladen werden')
@@ -31,82 +35,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.card{
-  background-color: white;
-  color: $color-dark-text;
-  padding: 10px 25px;
-  text-align: left;
-  text-decoration: none;
-  display: block;
-  font-size: 14px;
-  border: 2px solid $color-info;
-  border-radius: 5px;
-  margin: 8px;
-  overflow: hidden;
-  position: relative;
-
-  &.warning {
-    background-color: $color-warning;
-    color: $color-dark-text;
-  }
-
-  &.danger {
-    background-color: $color-danger;
-  }
-
-  &:hover {
-    box-shadow: 0 4px 4px 0 $color-info;
-  }
+tr{
+    text-align: inherit;
 }
-
-.header{
-    font-size: 18px;
-    text-align: left;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 14px;
-    font-weight: bold;
-    width:100%;
-    min-height: 40px;
-    padding: 16px;
-    position: relative;
+td{
+    text-align: inherit;
+    display: table-cell;
+    width: 30em;
 }
-.header .primary-title{
-  bottom: auto;
-  display: inline-block;
-  padding: 0;
-  position: absolute;
-  top: 50%;
-  -moz-transform: translate(0, -50%);
-  -ms-transform: translate(0, -50%);
-  -o-transform: translate(0, -50%);
-  -webkit-transform: translate(0, -50%);
-  transform: translate(0, -50%);
-}
-.title {
-  font-size: 14px;
-  font-weight:bold;
-  display: inline-block;
-  margin: 10px;
-}
-.title + .subhead {
-  margin-top: 7px;
-  color: $color-light-text;
-}
-.body{
-    color:$color-light-text;
-    padding: 16px;
-    text-align: left;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 14px;
-    width:100%;
-    line-height: 1.5;
-    border: 1px solid $color-mono-light;
-    border-width: 0 0 1px;
-}
-
-Button{
-    margin: 10px 20px;
+.min {
+    width: 1%;
+    white-space: nowrap;
 }
 </style>
