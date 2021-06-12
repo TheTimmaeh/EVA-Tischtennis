d6ac438eade74040c9a30a87b25b6a6ce31955e0
Tim Elbert <tim.elbert@iem.thm.de>
2021-06-12 22:22:16 +0200

    QueryBuilder for DB

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -1,4 +1,20 @@
 const knex = require('knex')
 const db = knex(require('./knexfile')[process.env.NODE_ENV])
 
+db.qb = (data) => {
+  let query = db
+
+  if(data.select) query = query.select(data.select)
+  if(data.first) query = query.first(data.first)
+  if(data.from) query = query.from(data.from)
+  if(data.filter)
+    if(data.filter instanceof Array) query = query.where(...data.filter)
+    else if(data.filter instanceof Object) query = query.where(data.filter)
+  if(data.order) query = query.orderBy(data.order)
+  if(data.limit) query = query.limit(data.limit)
+  if(data.offset) query = query.offset(data.offset)
+
+  return query
+}
+
 module.exports = db
