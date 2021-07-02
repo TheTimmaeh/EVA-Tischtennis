8c714271bb70a47f880a90264fd5bb5aae234292
Tina <tina.haensel@iem.thm.de>
2021-07-02 14:14:40 +0200

    Reworked AssociationProfile

--- frontend-private/src/views/Associations/Profile.vue
+++ frontend-private/src/views/Associations/Profile.vue
@@ -4,22 +4,32 @@
     <router-link :to="`/associations/${$route.params.id}/teams`">Mannschaften</router-link>
 
     {{ $route.params.id }}<br /><br />
+    
 
+    <router-link :to="`/associations/${$route.params.id}/teams/Create`">
+      <Button id="myButton" >Team anlegen</Button> <br>
+    </router-link><br/>
+    <router-link :to="`/associations/${$route.params.id}/members/Create`">
+      <Button id="myButton" >Mitglied anlegen</Button> <br>
+    </router-link>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 import { api, setTitle } from '@/helper'
+import Button from '@/components/Button'
 
 export default {
   name: 'Associations',
   components: {
+    Button,
   },
   setup(){
     setTitle('Verein')
 
     return {
+      Button,
     }
   },
 }
