db1783a96277e6d48d1f5c10046bb24cef07048a
Tim Elbert <tim.elbert@iem.thm.de>
2021-07-24 20:01:07 +0200

    Changes in Router regarding Competition Teams

---
+++ frontend-private/src/views/Competitions/Teams.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="team">
+    Teams
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CompetitionsTeams',
+  components: {},
+  setup(){
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.list{
+  padding: 20px;
+}
+</style>
