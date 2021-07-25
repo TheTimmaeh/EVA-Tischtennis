40e5205408d8ecc80f326b9410256ec6f6294f23
Tina <tina.haensel@iem.thm.de>
2021-07-25 18:58:38 +0200

    Reworked EncountersList

--- frontend-private/src/views/Competitions/Encounters/Index.vue
+++ frontend-private/src/views/Competitions/Encounters/Index.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="encounters">
     <div class="list">Liste der Begegnungen</div>
+       
+    <EncountersCard v-for="encounter in encounters" :key="encounter.id" :data="encounter"></EncountersCard>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
-import { api, setTitle } from '@/helper'
+import { useRouter, useRoute } from 'vue-router'
+import { api, setTitle, validate } from '@/helper'
 import Button from '@/components/FormElements/Button'
+import EncountersCard from '@/components/Cards/EncountersCard'
 
 export default {
   name: 'Encounters',
   components: {
     Button,
+    EncountersCard,
   },
   setup(){
+    const route = useRoute()
     setTitle('Begegnungen')
 
     const encounters = ref([])
 
-    api('/encounters').then((res) => res.data).then((res) => {
+    api(`/competitions/${route.params.competitionId}/encounters`).then((res) => res.data).then((res) => {
       if(!res.success){
         console.error('Fehler...', res)
         return
