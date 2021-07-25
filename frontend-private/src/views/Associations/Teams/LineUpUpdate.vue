02593ac24f0acd2894387595cb026e54b53cca23
Tina <tina.haensel@iem.thm.de>
2021-07-25 18:57:51 +0200

    Reworked LineUp Update

--- frontend-private/src/views/Associations/Teams/LineUpUpdate.vue
+++ frontend-private/src/views/Associations/Teams/LineUpUpdate.vue
@@ -22,19 +22,20 @@
       const route = useRoute()
       const rows = ref()
 
-      api(`/lineup/${route.params.teamId}`).then((res) => res.data).then((res) => {
+
+      api(`/associations/${route.params.associationId}/teams/${route.params.teamId}/members`).then((res) => res.data).then((res) => {
         if(!res.success){
           console.error('Fehler...', res)
           return
         }
         rows.value = [
           { name: 'member', text: 'Mannschaftsaufstellung', field: 'h2' },
-          { name: 'position1', text: 'Spieler 1:', field: 'search', apiPath: '/persons', displayFormat: '{{ name }}', lookupRow: ['name'], returnPath: 'id', validate: { required: true } },
-          { name: 'position1', text: 'Spieler 2:', field: 'search', apiPath: '/persons', displayFormat: '{{ name }}', lookupRow: ['name'], returnPath: 'id', validate: { required: true } },
-          { name: 'position1', text: 'Spieler 3:', field: 'search', apiPath: '/persons', displayFormat: '{{ name }}', lookupRow: ['name'], returnPath: 'id', validate: { required: true } },
-          { name: 'position1', text: 'Spieler 4:', field: 'search', apiPath: '/persons', displayFormat: '{{ name }}', lookupRow: ['name'], returnPath: 'id', validate: { required: true } },
-          { name: 'position1', text: 'Spieler 5:', field: 'search', apiPath: '/persons', displayFormat: '{{ name }}', lookupRow: ['name'], returnPath: 'id', validate: { required: true } },
-        ]
+          { name: 'E1', text: 'Spieler 1 - E1:', field: 'search', apiPath: '/persons', displayFormat: '{{ name }}', lookupRow: ['name'], returnPath: 'id', validate: { required: true }, value:res.data.E1 },
+          { name: 'E2', text: 'Spieler 2 -  E2:', field: 'search', apiPath: '/persons', displayFormat: '{{ name }}', lookupRow: ['name'], returnPath: 'id', validate: { required: true }, value:res.data.E2 },
+          { name: 'E3', text: 'Spieler 3 -  E3:', field: 'search', apiPath: '/persons', displayFormat: '{{ name }}', lookupRow: ['name'], returnPath: 'id', validate: { required: true }, value:res.data.E3 },
+          { name: 'DD1', text: 'Spieler 4 -  DD1:', field: 'search', apiPath: '/persons', displayFormat: '{{ name }}', lookupRow: ['name'], returnPath: 'id', validate: { required: true }, value:res.data.DD1},
+          { name: 'DD2', text: 'Spieler 5 -  DD2:', field: 'search', apiPath: '/persons', displayFormat: '{{ name }}', lookupRow: ['name'], returnPath: 'id', validate: { required: true }, value:res.data.DD2 },
+         ]
       })
 
       const message = ref('')
@@ -42,14 +43,14 @@
       const submit = (data) => {
         message.value = ''
 
-        api({ method: 'POST', path: `/lineup/${route.params.teamId}`, data }).then((res) => {
+        api({ method: 'POST', path: `/associations/${route.params.associationId}/teams/${route.params.teamId}/membersUpdate`, data }).then((res) => {
           if(!res.data){
             message.value = 'Unknown Error.'
           } else if(!res.data.success){
             message.value = res.data.message
           } else {
             message.value = 'Mannschaftsaufstellung wurde aktualisiert.'
-            setTimeout(() => router.push({ path: `/associations/${route.params.associationId}/teams` }), 3000)
+            setTimeout(() => router.push({ path: `/associations/${route.params.associationId}/teams/` }), 3000)
           }
         }).catch((err) => {
           message.value = err
