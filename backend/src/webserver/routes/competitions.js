9472e9f95ea976b5f880c6d295cfb81c44c425ff
Tim Elbert <tim.elbert@iem.thm.de>
2021-07-31 16:23:10 +0200

    Split Competitions/Encouters/Matches

--- backend/src/webserver/routes/competitions.js
+++ backend/src/webserver/routes/competitions.js
@@ -14,7 +14,7 @@ module.exports = (_db) => {
   router.get('/:competitionId', getCompetition)
 
   // Create
-  router.post('/', optionalAuthenticateToken,  createCompetition)
+  router.post('/', authenticateToken,  createCompetition)
 
   // Update
   router.post('/:competitionId', authenticateToken, updateCompetition)
@@ -25,29 +25,8 @@ module.exports = (_db) => {
   // AssociationTeam Search
   router.get('/:competitionId/association_teams', authenticateToken, getAssociationTeams)
 
-  // Encounters Create
-  router.post('/:competitionId/encounters/create', authenticateToken, createEncounters)
-
-  // Get All Encounters 
-  router.get('/:competitionId/encounters', authenticateToken, getAllEncounters)
-
-  // Get  Encounter
-  router.get('/:competitionId/encounters/:encountersId', authenticateToken, getEncounter)
-
-  // Encounters Update
-  router.post('/:competitionId/encounters/:encountersId/update', authenticateToken, updateEncounters)
-
-   // Encounter Delete
-   router.delete('/:competitionId/encounters/:encountersId', authenticateToken, deleteEncounter)
-
-  // Match Create
-  router.post('/:competitionId/encounters/:encountersId/match/create', authenticateToken, createMatch)
-
-  // Get All Matches
-  router.get('/:competitionId/encounters/:encountersId/match/index', authenticateToken, getAllMatches)
-
-  // Delete Match
-  router.delete('/:competitionId/encounters/:encountersId/match/:matchId', authenticateToken, deleteMatch)
+  // Encounters
+  router.use('/:competitionId/encounters', require('./encounters')(db))
 
   return router
 }
@@ -136,7 +115,7 @@ async function updateCompetition(req, res){
 }
 
 async function deleteCompetition(req, res){
-  
+
   try {
     result = await db('competitions').where({ id: req.params.competitionId }).del()
   } catch(err){
@@ -169,168 +148,4 @@ async function getAssociationTeams(req, res){
     console.error(err.message)
     res.status(500).json({ success: false, message: `An error has occured. (${err.code})` })
   }
-}
-
-
-async function createEncounters(req, res){
-  let result
-
-  try {
-    result = await db('encounters').insert({
-      competition: req.params.competitionId,
-      game_stage: req.body.gamestage,
-      home: req.body.home,
-      visitor: req.body.visitor,
-      datetime: req.body.date,
-    })
-    console.log(result)
-  } catch(err){
-    if(err.code === 'ER_DUP_ENTRY'){
-      res.json({ success: false, message: `Es existiert bereits eine Begegnung zwischen ${req.body.home} ${req.body.visitor}.` })
-    } else {
-      res.json({ success: false, message: `Ein unbekannter Fehler ist aufgetreten. (${err.code})` })
-      console.error({ ...err })
-    }
-    console.error(err)
-
-    return
-  }
-
-  if(!!result?.[0]){
-    let data = await db('competitions').where({ id: result[0] }).first()
-
-    res.json({ success: true, message: 'Turnier wurde angelegt.', data })
-  } else {
-    res.json({ success: false, message: 'Ein unbekannter Fehler ist aufgetreten. (2)' })
-  }
-}
-
-async function getAllEncounters(req, res){
-  try {
-    let data = await db.qb({ select: (req.user?.isAdmin ? select.admin : select.guest), from: 'encounters', ...req.query })
-
-    res.json({ success: true, data })
-  } catch(err){
-    console.error(err.message)
-    res.status(500).json({ success: false, message: `An error has occured. (${err.code})` })
-  }
-}
-
-async function getEncounter(req, res){
-  let data = (await db.first(req.user?.isAdmin ? select.admin : select.guest).from('encounters').where({ id: req.params.encountersId }))
-  res.json({ success: true, data })
-}
-
-async function updateEncounters(req, res){
-  let result
-
-  try {
-    result = await db('encounters').where({ id: req.params.encontersId }).update({
-      competition: req.params.competitionId,
-      game_stage: req.body.gamestage,
-      home: req.body.home,
-      visitor: req.body.visitor,
-      datetime: req.body.date,
-    })
-  } catch(err){
-    if(err.code === 'ER_DUP_ENTRY'){
-      res.json({ success: false, message: `Es existiert bereits eine solche Begegnung.` })
-    } else {
-      res.json({ success: false, message: `Ein unbekannter Fehler ist aufgetreten. (${err.code})` })
-      console.error({ ...err })
-    }
-
-    return
-  }
-
-  if(result === 1){
-    let data = (await db.first(select.admin).from('encounters').where({ id: req.params.encontersId }))
-
-    res.json({ success: true, message: 'Begegnungsdaten wurden aktualisiert.', data })
-  } else {
-    res.json({ success: false, message: 'Ein unbekannter Fehler ist aufgetreten. (4)' })
-  }
-}
-
-async function deleteEncounter(req, res){
-  console.log(req.params.encountersId)
-  
-  try {
-    result = await db('encounters').where({ id: req.params.encountersId }).del()
-  } catch(err){
-    if(err.code === 'ER_ROW_IS_REFERENCED_2'){
-      res.json({ success: false, message: `Diese Begegnung ist in Benutzung und kann daher nicht gelöscht werden!` })
-    } else {
-      res.json({ success: false, message: `Ein unbekannter Fehler ist aufgetreten. (${err.code})` })
-      console.error({ ...err })
-    }
-    return
-  }
-
-  res.json({ success: true, message: 'Die Daten der Begegnung wurden gelöscht.' })
-
-}
-
-
-async function createMatch(req, res){
-  let result
-
-  try {
-    result = await db('matches').insert({
-      home_player: req.body.home_player,
-      visitor_player: req.body.visitor_player,
-      referee: req.body.referee,
-      encounter: req.params.encontersId,
-      sets: 5,
-    })
-    console.log(result)
-  } catch(err){
-    if(err.code === 'ER_DUP_ENTRY'){
-      res.json({ success: false, message: `Es existiert bereits eine solches Spiel zwischen ${req.body.home_player} ${req.body.visitor_player}.` })
-    } else {
-      res.json({ success: false, message: `Ein unbekannter Fehler ist aufgetreten. (${err.code})` })
-      console.error({ ...err })
-    }
-    console.error(err)
-
-    return
-  }
-
-  if(!!result?.[0]){
-    let data = await db('matches').where({ id: result[0] }).first()
-
-    res.json({ success: true, message: 'Spiel wurde angelegt.', data })
-  } else {
-    res.json({ success: false, message: 'Ein unbekannter Fehler ist aufgetreten. (2)' })
-  }
-}
-
-
-async function getAllMatches(req, res){
-  try {
-    let data = await db.qb({ select: (req.user?.isAdmin ? select.admin : select.guest), from: 'matches', ...req.query })
-
-    res.json({ success: true, data })
-  } catch(err){
-    console.error(err.message)
-    res.status(500).json({ success: false, message: `An error has occured. (${err.code})` })
-  }
-}
-
-async function deleteMatch(req, res){
-  
-  try {
-    result = await db('matches').where({ id: req.params.matchId }).del()
-  } catch(err){
-    if(err.code === 'ER_ROW_IS_REFERENCED_2'){
-      res.json({ success: false, message: `Dieses Spiel ist in Benutzung und kann daher nicht gelöscht werden!` })
-    } else {
-      res.json({ success: false, message: `Ein unbekannter Fehler ist aufgetreten. (${err.code})` })
-      console.error({ ...err })
-    }
-    return
-  }
-
-  res.json({ success: true, message: 'Die Daten des Spiels wurden gelöscht.' })
-
 }
---
+++ backend/src/webserver/routes/encounters.js
@@ -0,0 +1,180 @@
+const router = require('express').Router({ mergeParams: true })
+let db
+
+module.exports = (_db) => {
+
+  db = _db
+
+  const { generateToken, authenticateToken, optionalAuthenticateToken } = require('../../helpers/jwt')(db)
+
+  // Index
+  router.get('/', optionalAuthenticateToken, getAllEncounters)
+
+  // Get (one encounter)
+  router.get('/:encounterId', optionalAuthenticateToken, getEncounter)
+
+  // Create
+  router.post('/create', authenticateToken, createEncounter)
+
+  // Update
+  router.post('/:encounterId/update', authenticateToken, updateEncounter)
+
+  // Delete
+  router.delete('/:encounterId', authenticateToken, deleteEncounter)
+
+  // Matches
+  router.use('/:encounterId/matches', require('./matches')(db))
+
+  return router
+}
+
+const select = {
+  admin: ['*'],
+  guest: ['*'],
+}
+
+async function createEncounter(req, res){
+  let result
+
+  try {
+    result = await db('encounters').insert({
+      competition: req.params.competitionId,
+      game_stage: req.body.gamestage,
+      home: req.body.home,
+      visitor: req.body.visitor,
+      datetime: req.body.date,
+    })
+  } catch(err){
+    if(err.code === 'ER_DUP_ENTRY'){
+      res.json({ success: false, message: `Es existiert bereits eine Begegnung zwischen ${req.body.home} ${req.body.visitor}.` })
+    } else {
+      res.json({ success: false, message: `Ein unbekannter Fehler ist aufgetreten. (${err.code})` })
+      console.error({ ...err })
+    }
+
+    return
+  }
+
+  if(!!result?.[0]){
+    let encounter = await db('encounters').where({ id: result[0] }).first()
+    let home
+    let visitor
+
+    try {
+      home = await db('team_members').where({ team: encounter.home })
+      visitor = await db('team_members').where({ team: encounter.visitor })
+    } catch(err){
+      console.error(err)
+    }
+
+    try {
+      result = await db('matches').insert([
+        {
+          encounter: encounter.id,
+          home_player_1: home.find((p) => p.position == 'E1').member,
+          visitor_player_1: visitor.find((p) => p.position == 'E2').member,
+          sets: 5
+        },
+        {
+          encounter: encounter.id,
+          home_player_1: home.find((p) => p.position == 'E2').member,
+          visitor_player_1: visitor.find((p) => p.position == 'E1').member,
+          sets: 5
+        },
+        {
+          encounter: encounter.id,
+          home_player_1: home.find((p) => p.position == 'E3').member,
+          visitor_player_1: visitor.find((p) => p.position == 'E3').member,
+          sets: 5
+        },
+        {
+          encounter: encounter.id,
+          home_player_1: home.find((p) => p.position == 'D1').member,
+          home_player_2: home.find((p) => p.position == 'D2').member,
+          visitor_player_1: visitor.find((p) => p.position == 'D1').member,
+          visitor_player_2: visitor.find((p) => p.position == 'D2').member,
+          sets: 5
+        },
+      ])
+    } catch(err){
+      res.json({ success: false, message: `Ein unbekannter Fehler ist aufgetreten. (${err.code})` })
+      console.error({ ...err })
+    }
+
+    let matches
+
+    if(result.length > 0){
+      matches = await db('matches').whereIn('id', result)
+    }
+
+    console.log({ encounter, matches })
+
+    res.json({ success: true, message: 'Turnier wurde angelegt.', data: { encounter, matches } })
+  } else {
+    res.json({ success: false, message: 'Ein unbekannter Fehler ist aufgetreten. (2)' })
+  }
+}
+
+async function getAllEncounters(req, res){
+  try {
+    let data = await db.qb({ select: (req.user?.isAdmin ? select.admin : select.guest), from: 'encounters', ...req.query })
+
+    res.json({ success: true, data })
+  } catch(err){
+    console.error(err.message)
+    res.status(500).json({ success: false, message: `An error has occured. (${err.code})` })
+  }
+}
+
+async function getEncounter(req, res){
+  let data = (await db.first(req.user?.isAdmin ? select.admin : select.guest).from('encounters').where({ id: req.params.encounterId }))
+  res.json({ success: true, data })
+}
+
+async function updateEncounter(req, res){
+  let result
+
+  try {
+    result = await db('encounters').where({ id: req.params.encontersId }).update({
+      competition: req.params.competitionId,
+      game_stage: req.body.gamestage,
+      home: req.body.home,
+      visitor: req.body.visitor,
+      datetime: req.body.date,
+    })
+  } catch(err){
+    if(err.code === 'ER_DUP_ENTRY'){
+      res.json({ success: false, message: `Es existiert bereits eine solche Begegnung.` })
+    } else {
+      res.json({ success: false, message: `Ein unbekannter Fehler ist aufgetreten. (${err.code})` })
+      console.error({ ...err })
+    }
+
+    return
+  }
+
+  if(result === 1){
+    let data = (await db.first(select.admin).from('encounters').where({ id: req.params.encontersId }))
+
+    res.json({ success: true, message: 'Begegnungsdaten wurden aktualisiert.', data })
+  } else {
+    res.json({ success: false, message: 'Ein unbekannter Fehler ist aufgetreten. (4)' })
+  }
+}
+
+async function deleteEncounter(req, res){
+  try {
+    result = await db('encounters').where({ id: req.params.encounterId }).del()
+  } catch(err){
+    if(err.code === 'ER_ROW_IS_REFERENCED_2'){
+      res.json({ success: false, message: `Diese Begegnung ist in Benutzung und kann daher nicht gelöscht werden!` })
+    } else {
+      res.json({ success: false, message: `Ein unbekannter Fehler ist aufgetreten. (${err.code})` })
+      console.error({ ...err })
+    }
+    return
+  }
+
+  res.json({ success: true, message: 'Die Daten der Begegnung wurden gelöscht.' })
+
+}
