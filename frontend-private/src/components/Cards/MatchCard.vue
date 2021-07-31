db093f9d1951f65ae64efc0e6bb66bb0c84a7dd7
Tim Elbert <tim.elbert@iem.thm.de>
2021-07-31 16:24:02 +0200

    Added Icons to MatchCard

--- frontend-private/src/components/Cards/MatchCard.vue
+++ frontend-private/src/components/Cards/MatchCard.vue
@@ -1,14 +1,18 @@
 <template>
   <Card>
     <tr>
-      <td>Heimspieler: {{home_player}}</td>
-      <td>Gastpieler: {{visitor_player}}</td>
-      <td>Schiedsricher: {{referee}}</td>
-      <td>Punktestand Heim: {{home_score}}</td>
-      <td>Punktestand Gast: {{visitor_score}}</td>
-      <td>
+      <td>Heimspieler: {{ home_player }}</td>
+      <td>Gastpieler: {{ visitor_player }}</td>
+      <td>Schiedsricher: {{ referee }}</td>
+      <td class="score min pleft">{{ home_score }}0</td>
+      <td class="score min pcenter">:</td>
+      <td class="score min pright">{{ visitor_score }}3</td>
+      <td class="min">
+        <router-link :to="`/competitions/${$route.params.competitionId}/encounters/${$route.params.encounterId}/matches/${id}/update`">
+          <Button><Icon type="edit" /></Button>
+        </router-link>
         <router-link :to="`/matches/${id}/delete`">
-            <Button level="danger">Löschen</Button>
+          <Button level="danger"><Icon type="delete" /></Button>
         </router-link>
       </td>
     </tr>
@@ -20,6 +24,7 @@ import { useRouter, useRoute } from 'vue-router'
 import { api, setTitle, validate } from '@/helper'
 import Button from '@/components/FormElements/Button'
 import Card from '@/components/Cards/Card'
+import Icon from '@/components/Icons/Icon'
 import { ref } from 'vue'
 
 export default {
@@ -27,7 +32,8 @@ export default {
   components:{
     Button,
     Card,
-    },
+    Icon,
+  },
   props: {
       data: {
         type: Object,
@@ -55,5 +61,24 @@ td{
 .min {
     width: 1%;
     white-space: nowrap;
+}
+
+.score {
+  font-size: 200%;
+  font-weight: 700;
+  vertical-align: middle;
+}
+
+.pleft {
+  padding-left: 20px;
+}
+
+.pcenter {
+  padding-right: 5px;
+  padding-left: 5px;
+}
+
+.pright {
+  padding-right: 20px;
 }
 </style>
