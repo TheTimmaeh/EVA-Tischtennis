d6aacc9c256f60c272d17d221f4d98898254d5ac
Tina <tina.haensel@iem.thm.de>
2021-07-31 09:58:17 +0200

    AssociationProfile Data added

--- frontend-private/src/views/Associations/Profile.vue
+++ frontend-private/src/views/Associations/Profile.vue
@@ -1,25 +1,59 @@
 <template>
   <div class="association">
-    Das sind die Vereinsstammdaten.
+    <Card>
+        <div class="header">
+          <div class="primary-title">
+            <div class="title"><b>{{ association.name }}</b></div>
+          </div>
+        </div>
+        <div class="body">
+          <tr><b>Gründungsjahr:</b> {{association.year}}  </tr>
+          <tr><b>Adresse:</b><br>
+              {{association.street}} {{association.streetnumer}}<br>
+              {{association.zipcode}} {{association.city}}<br>
+              {{association.state}} - {{association.country}}<br>
+          </tr>
+          <tr><b>Mail:</b> {{association.mail}} <br>
+          </tr>
+          <tr><b>Website:</b> {{association.url}} <br>
+          </tr>
+        </div>
+    </Card>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 import { api, setTitle } from '@/helper'
 import { useRoute } from 'vue-router'
 import Button from '@/components/FormElements/Button'
+import Card from '@/components/Cards/Card'
 
 export default {
   name: 'AssociationsProfile',
   components: {
     Button,
+    Card,
   },
   setup(){
     const route = useRoute()
     setTitle(`Verein ${route.params.associationId}`)
 
+      const association = ref({})
+
+      api(`/associations/${route.params.associationId}`).then((res) => res.data).then((res) => {
+        if(!res.success){
+          console.error('Fehler...', res)
+          return
+        }
+
+        association.value = res.data
+      })
+
+
     return {
       Button,
+      association,
     }
   },
 }
@@ -27,6 +61,13 @@ export default {
 
 <style lang="scss" scoped>
 .list{
+  padding: 20px;
+}
+tr{
+  padding: 20px;
+  margin: 10px;
+}
+.body{
   padding: 20px;
 }
 </style>
