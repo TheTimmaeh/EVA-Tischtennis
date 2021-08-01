d8d7ccd94e91a46d969d790aba2adcdf7a733dca
Tim Elbert <tim.elbert@iem.thm.de>
2021-08-01 16:24:41 +0200

    Page Component

---
+++ frontend-private/src/components/Page.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot><router-view></router-view></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Page',
+  }
+</script>
+
+<style lang="scss" scoped>
+  div {
+    width: 960px;
+    margin: 0 auto;
+  }
+</style>
