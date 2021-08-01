649c00025abc0894961276916837bebe0d7be8eb
Tina <tina.haensel@iem.thm.de>
2021-08-01 10:46:49 +0200

    Added Board to Associations Profile and Reworked Profile

--- frontend-private/src/views/Associations/Profile.vue
+++ frontend-private/src/views/Associations/Profile.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="association">
-    <Card>
+     <Card>
         <div class="header">
           <div class="primary-title">
-            <div class="title"><b>{{ association.name }}</b></div>
+            <div class="title">
+              <div class="name">{{ association.name}}</div>
+              <router-link  :to="`/persons/${association.board}/profile`">
+                <Button>Vorsitzender</Button>
+              </router-link>  
+           </div>
           </div>
         </div>
         <div class="body">
-          <tr><b>Gründungsjahr:</b> {{association.year}}  </tr>
-          <tr><b>Adresse:</b><br>
-              {{association.street}} {{association.streetnumer}}<br>
-              {{association.zipcode}} {{association.city}}<br>
-              {{association.state}} - {{association.country}}<br>
+          <tr>
+            <td>Gründungsjahr: {{ association.year }}</td>
           </tr>
-          <tr><b>Mail:</b> {{association.mail}} <br>
+          <tr>
+              <td :v-if='association.street' >Adresse: {{ association.street }} {{ association.streetnumber }} | {{ association.zipcode }} {{ association.city }} | {{ association.state }} {{ association.country }}</td>
           </tr>
-          <tr><b>Website:</b> {{association.url}} <br>
+          <tr>
+              <td>Kontakt: {{ association.mail }} <br> {{ association.url }} </td>
+          </tr>
+          <tr>
+              <td>Beschreibung: {{ association.description }}  </td> <br>
           </tr>
         </div>
     </Card>
@@ -35,6 +42,11 @@ export default {
     Button,
     Card,
   },
+   props: {
+      association: {
+        type: Object,
+      },
+    },
   setup(){
     const route = useRoute()
     setTitle(`Verein ${route.params.associationId}`)
@@ -50,7 +62,6 @@ export default {
         association.value = res.data
       })
 
-
     return {
       Button,
       association,
@@ -60,14 +71,65 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.list{
-  padding: 20px;
+
+.header{
+    font-size: 18px;
+    text-align: left;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 14px;
+    font-weight: bold;
+    width:100%;
+    min-height: 60px;
+    padding: 16px;
+    position: relative;
+    border: 1px solid $color-mono-light;
+    border-width: 0 0 1px;
 }
-tr{
-  padding: 20px;
-  margin: 10px;
+.header .primary-title{
+  bottom: auto;
+  display: inline-block;
+  padding: 0;
+  position: absolute;
+  top: 50%;
+  -moz-transform: translate(0, -50%);
+  -ms-transform: translate(0, -50%);
+  -o-transform: translate(0, -50%);
+  -webkit-transform: translate(0, -50%);
+  transform: translate(0, -50%);
+}
+.title {
+  font-size: 14px;
+  font-weight:bold;
+  padding: 15px;
 }
 .body{
-  padding: 20px;
+    color:$color-light-text;
+    padding: 16px;
+    text-align: left;
+    text-decoration: none;
+    font-size: 14px;
+    width: 120%;
+    line-height: 1.5;
+    border-width: 0 0 1px;
+}
+td{
+    text-align: inherit;
+    display: table-cell;
+    width: 30em;
+}
+tr{
+    text-align: inherit;
+}
+.min {
+    width: 1%;
+    white-space: nowrap;
+}
+.name{
+  display:inline-block;
+  padding: 16px;
+}
+button{
+  margin: 80px 16px;
 }
 </style>
