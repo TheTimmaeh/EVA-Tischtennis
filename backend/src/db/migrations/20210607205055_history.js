459732865a7a900bdb222876eac6f3fa32dd74ad
Tim Elbert <tim.elbert@iem.thm.de>
2021-08-06 19:02:54 +0200

    History Migration Fix

--- backend/src/db/migrations/20210607205055_history.js
+++ backend/src/db/migrations/20210607205055_history.js
@@ -1,9 +1,9 @@
 exports.up = (knex) => {
   return knex.schema.createTable('history', (history) => {
     history.increments('id')
-    history.string('event_type', 255).unique().notNullable()
+    history.string('event_type', 255).notNullable()
     history.string('reference1', 255).notNullable()
-    history.string('reference2', 255).notNullable()
+    history.string('reference2', 255).nullable()
     history.integer('user').unsigned().references('users.id').notNullable()
     history.timestamps(true, true)
     console.info('DB | Migration: history | Table history created.')
