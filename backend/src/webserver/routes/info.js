f276e7071c5dcfae52abdf736a8702e6ca107ed1
Tim Elbert <tim.elbert@iem.thm.de>
2021-08-10 15:11:17 +0200

    Extended Info Route

--- backend/src/webserver/routes/info.js
+++ backend/src/webserver/routes/info.js
@@ -6,14 +6,28 @@ module.exports = (_db) => {
 
   db = _db
 
-  // Index
-  router.get('/', info)
+  router.get('/', async function(req, res){
+
+    let database
+
+    try {
+      let result = await db.raw('SELECT @@server_id AS serverId, @@version AS version, @@hostname AS hostname, @@port AS port, SUM(data_length + index_length) AS "bytes" FROM information_schema.TABLES GROUP BY table_catalog')
+
+      while(result instanceof Array) result = result[0]
+
+      database = result
+    } catch(err){
+      console.error(err)
+    }
+
+    res.json({
+      backend: {
+        version: process.version,
+        hostname: os.hostname(),
+      },
+      database,
+    })
+  })
 
   return router
-}
-
-async function info(req, res){
-  res.json({
-    dockerId: os.hostname(),
-  })
 }
