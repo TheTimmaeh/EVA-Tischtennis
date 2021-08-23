1acbb4b89dd136967c584ce25af1324ab3109293
Tim Elbert <tim.elbert@iem.thm.de>
2021-08-23 17:30:53 +0200

    Match Seed Fix

--- backend/src/db/seeds/07_matches.js
+++ backend/src/db/seeds/07_matches.js
@@ -1,30 +1,30 @@
 exports.seed = (knex) => {
-  return knex('encounters').del().then(() => {
-    return knex('encounters').insert([
+  return knex('matches').del().then(() => {
+    return knex('matches').insert([
       {
         id: 1,
-        encounter: 2,
+        encounter: 1,
         home_player_1: 2,
         visitor_player_1: 71,
         sets: 5,
       },
       {
         id: 2,
-        encounter: 2,
+        encounter: 1,
         home_player_1: 5,
         visitor_player_1: 70,
         sets: 5,
       },
       {
         id: 3,
-        encounter: 2,
+        encounter: 1,
         home_player_1: 8,
         visitor_player_1: 72,
         sets: 5,
       },
       {
         id: 4,
-        encounter: 2,
+        encounter: 1,
         home_player_1: 9,
         home_player_2: 10,
         visitor_player_1: 75,
